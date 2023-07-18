4610bfc3762c9d0bc65fd96973ceed182e06b58c
naren.b <narenbagavathyhi5@gmail.com>
2023-07-18 13:34:06 +0530

    Initial commit - Started working on Hirst Painting Project

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+                                              Hirst Painting Project
+
+                  colorgram.py is a Python Library that lets you extract colors from images.
+                  Compared to other libraries, the colorgram algorithm's results are more intense.
+'''
+
+
+
+
+# TODO 1: Extracts RGB Values From Images
+
+
+
+# TODO 2: Drawing the dots
+
